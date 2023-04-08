04d266ee40b17c479ca68ccc51c46c124d707378
Ishita <iishita_be22@thapar.edu>
2023-04-08 17:06:55 +0530

    changed scanner screen

--- lib/Retailer/add_items.dart
+++ lib/Retailer/add_items.dart
@@ -10,140 +10,109 @@ class AddItemsScreen extends StatefulWidget {
 }
 
 class _AddItemsScreenState extends State<AddItemsScreen> {
-  @override
-  Widget build(BuildContext context) {
-    FlutterBarcodeScanner.scanBarcode(
-            "#ff6666", "Cancel", false, ScanMode.DEFAULT)!
-        .then((barcode) {
-      Fluttertoast.showToast(msg: barcode);
-      processBarcode(barcode, context);
-    });
-    return const Placeholder();
-  }
-}
-
-void processBarcode(String barcode, BuildContext context) async {
-  MyUtils.showLoaderDialog(context);
-  final itemSnapshot = await checkIfItemExists(barcode);
-  final shopItemSnapshot = await checkIfItemExistsForShop(barcode);
+  bool isBarcodeScanned = false;
   TextEditingController nameController = TextEditingController(),
       manufacturerController = TextEditingController(),
       mrpController = TextEditingController(),
       priceController = TextEditingController(),
       stockController = TextEditingController();
-  AlertDialog alert = AlertDialog(
-    content: Container(
-      margin: EdgeInsets.all(16),
-      child: Center(
-        child: Column(
-          children: [
-            Text("${shopItemSnapshot.exists ? 'Change' : 'Add'} Item"),
-            Container(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-              decoration: const BoxDecoration(
-                  //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
-                  ),
-              child: TextField(
-                onChanged: (value) {},
-                controller: nameController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Name",
-                    hintStyle: TextStyle(color: Colors.grey[400])),
-              ),
+  @override
+  Widget build(BuildContext context) {
+    if (!isBarcodeScanned) {
+      FlutterBarcodeScanner.scanBarcode(
+              "#ff6666", "Cancel", false, ScanMode.DEFAULT)
+          .then((barcode) {
+        isBarcodeScanned = true;
+        Fluttertoast.showToast(msg: barcode);
+        processBarcode(barcode, context);
+      });
+    }
+    return Center(
+      child: Column(
+        children: [
+          const Text("Add/Change Item"),
+          Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
+            decoration: const BoxDecoration(
+                //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
+                ),
+            child: TextField(
+              onChanged: (value) {},
+              controller: nameController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Name",
+                  hintStyle: TextStyle(color: Colors.grey[400])),
             ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-              decoration: const BoxDecoration(
-                  //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
-                  ),
-              child: TextField(
-                onChanged: (value) {},
-                controller: manufacturerController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Manufacturer",
-                    hintStyle: TextStyle(color: Colors.grey[400])),
-              ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
+            decoration: const BoxDecoration(
+                //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
+                ),
+            child: TextField(
+              onChanged: (value) {},
+              controller: manufacturerController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Manufacturer",
+                  hintStyle: TextStyle(color: Colors.grey[400])),
             ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-              decoration: const BoxDecoration(
-                  //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
-                  ),
-              child: TextField(
-                onChanged: (value) {},
-                controller: mrpController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "MRP",
-                    hintStyle: TextStyle(color: Colors.grey[400])),
-              ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
+            decoration: const BoxDecoration(
+                //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
+                ),
+            child: TextField(
+              onChanged: (value) {},
+              controller: mrpController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "MRP",
+                  hintStyle: TextStyle(color: Colors.grey[400])),
             ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-              decoration: const BoxDecoration(
-                  //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
-                  ),
-              child: TextField(
-                onChanged: (value) {},
-                controller: priceController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Your price",
-                    hintStyle: TextStyle(color: Colors.grey[400])),
-              ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
+            decoration: const BoxDecoration(
+                //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
+                ),
+            child: TextField(
+              onChanged: (value) {},
+              controller: priceController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Your price",
+                  hintStyle: TextStyle(color: Colors.grey[400])),
             ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-              decoration: const BoxDecoration(
-                  //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
-                  ),
-              child: TextField(
-                onChanged: (value) {},
-                controller: stockController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Stock",
-                    hintStyle: TextStyle(color: Colors.grey[400])),
-              ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
+            decoration: const BoxDecoration(
+                //border: Border(bottom: BorderSide(color: Colors.grey[400]))!
+                ),
+            child: TextField(
+              onChanged: (value) {},
+              controller: stockController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Stock",
+                  hintStyle: TextStyle(color: Colors.grey[400])),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-    ),
-    actions: [
-      TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text("Cancel")),
-      TextButton(onPressed: () {}, child: Text("Save"))
-    ],
-  );
-  await showDialog(
-    barrierDismissible: true,
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-  if (shopItemSnapshot.exists) {
-    stockController!.text = shopItemSnapshot.child('stock').value.toString();
-    priceController!.text = shopItemSnapshot.child('price').value.toString();
-  }
-  if (itemSnapshot.exists) {
-    nameController!.text = itemSnapshot.child('name').value.toString();
-    manufacturerController!.text =
-        itemSnapshot.child('manufacturer').value.toString();
-    mrpController!.text = itemSnapshot.child('mrp').value.toString();
+    );
   }
 }
+
+void processBarcode(String barcode, BuildContext context) async {}
 
 Future<DataSnapshot> checkIfItemExists(String barcode) async {
   final snapshot =
